ece07e549f7a1edcebbd91484c3329d1d5d13bf9
FabrizioCmp <fabriziocampari01@gmail.com>
2022-10-28 17:29:35 +0200

    buttons genera & annulla funzionanti

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@ const distance = document.getElementById("distance");
 const fullName = document.getElementById("fullname");
 const age = document.getElementById("age");
 const btnGenerateEl = document.getElementById("btn_generate");
+const btnDeletEl = document.getElementById("btn_delete");
 const nameEl = document.getElementById("name");
 const typeEl = document.getElementById("type");
 const cabEl = document.getElementById("cab");
 const codeEl = document.getElementById("code");
 const priceEl = document.getElementById("price");
+const detailsEl = document.getElementById("details")
 
 
 
-// compute price with discount on button click
+// compute price with discount on button(Genera) click
 btnGenerateEl.addEventListener("click", function () {
     const ageN = Number(age.value);
     const distanceN = Number(distance.value);
@@ -37,4 +39,16 @@ btnGenerateEl.addEventListener("click", function () {
     cabEl.innerHTML = Math.floor(Math.random() * 10) + 1;
     codeEl.innerHTML = Math.floor(1000 + Math.random() * 9000);
     priceEl.innerHTML = price;
+
+    detailsEl.classList.add("d-block");
+    detailsEl.classList.remove("d-none");
+
+});
+
+// Clear details section on button(Annulla) click
+
+btnDeletEl.addEventListener("click", function() {
+    detailsEl.classList.add("d-none");
+    detailsEl.classList.remove("d-block");
+
 });
