21339434b0a5d5151d4942808fb11109dbae2547
FabrizioCmp <fabriziocampari01@gmail.com>
2022-10-28 17:11:07 +0200

    mostra dettaglio bigleitto on click

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
 
-// variables declaration
+// variables-constants declaration
 const distance = document.getElementById("distance");
 const fullName = document.getElementById("fullname");
 const age = document.getElementById("age");
-const btnGenerate = document.getElementById("btn_generate");
+const btnGenerateEl = document.getElementById("btn_generate");
+const nameEl = document.getElementById("name");
+const typeEl = document.getElementById("type");
+const cabEl = document.getElementById("cab");
+const codeEl = document.getElementById("code");
+const priceEl = document.getElementById("price");
+
+
 
 // compute price with discount on button click
-btnGenerate.addEventListener("click", function () {
+btnGenerateEl.addEventListener("click", function () {
     const ageN = Number(age.value);
     const distanceN = Number(distance.value);
     let price = distanceN * 0.21;
+    let ticketType = "Biglietto Standard"
 
     console.log(ageN);
     console.log(distanceN);
 
     if (ageN === 0.8) {
         price = price * ageN;
+        ticketType = "Ridotto minorenni";
     } else if (ageN === 0.6) {
         price = price * 0.6;
+        ticketType = "Ridotto over-65";
     }
 
     console.log(" prezzo con sconto" ,price);
 
+    nameEl.innerHTML = fullName.value;
+    typeEl.innerHTML =  ticketType;
+    cabEl.innerHTML = Math.floor(Math.random() * 10) + 1;
+    codeEl.innerHTML = Math.floor(1000 + Math.random() * 9000);
+    priceEl.innerHTML = price;
 });
-
